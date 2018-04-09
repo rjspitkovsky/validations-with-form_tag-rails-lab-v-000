a5dcf94cbab5cf62fa7cd64d350458f043e5f1bd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-09 20:52:36 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,9 @@ class PostsController < ApplicationController
     if @post.valid?
     @post.update(post_params)
     redirect_to post_path(@post)
-  else
+    else
     render :edit
-  end
+    end
 
 
   end
